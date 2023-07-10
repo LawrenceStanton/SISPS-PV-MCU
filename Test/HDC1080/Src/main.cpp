fd93ef5654dfd365dee78d7f524267f99ed0b15f
Lawrence Stanton <52958935+LawrenceStanton@users.noreply.github.com>
2023-07-10 11:54:22 +0200

    Add setConfig operations and softReset to test.

--- Test/HDC1080/Src/main.cpp
+++ Test/HDC1080/Src/main.cpp
@@ -10,13 +10,15 @@
  * for typically valid results.
  *
  * A PASS is indicated by the LEDs flashing every 1s.
- * A FAIL is indicated by the LEDs being on solid on.
+ * A FAIL is indicated by the LEDs being on solid ON-OFF-ON.
  *
  * The test is run recursively in an infinite loop.
  */
 
 #include "main.h"
 #include "stm32g0xx_hal.h"
+
+#include <cmath>
 
 #include "HDC1080.hpp"
 #include "HDC1080_I2C.hpp"
@@ -50,26 +52,53 @@ int main() {
 	HAL_Delay(1000);
 
 	while (true) {
+		// Get all registers with default config.
 		temperature	   = hdc1080.getTemperature();
 		humidity	   = hdc1080.getHumidity();
 		deviceID	   = hdc1080.getDeviceID();
 		manufacturerID = hdc1080.getManufacturerID();
 		serialID	   = hdc1080.getSerialID();
 
-		if (temperature != -40.0 &&															  //
-			humidity != 0.0 &&																  //
-			deviceID.has_value() && deviceID.value() == deviceIDExpected &&					  //
-			manufacturerID.has_value() && manufacturerID.value() == manufacturerIDExpected && //
-			serialID.has_value()) {
+		bool getRegistersSuccessDefaultConfig = temperature != -40.0										 //
+											 && humidity != 0.0												 //
+											 && deviceID.has_value() && deviceID.value() == deviceIDExpected //
+											 && manufacturerID.has_value()
+											 && manufacturerID.value() == manufacturerIDExpected //
+											 && serialID.has_value();
+
+		bool setConfigAcquisitionModeSuccess = hdc1080.setAcquisitionMode(HDC1080::AcquisitionMode::SINGLE).has_value();
+		bool setConfigTemperatureResolutionSuccess =
+			hdc1080.setTemperatureResolution(HDC1080::TemperatureResolution::A_11BIT).has_value();
+		bool setConfigHumidityResolutionSuccess =
+			hdc1080.setHumidityResolution(HDC1080::HumidityResolution::A_8BIT).has_value()
+			&& hdc1080.setHumidityResolution(HDC1080::HumidityResolution::A_11BIT).has_value();
+		bool setConfigHeaterEnabledSuccess = hdc1080.setHeater(HDC1080::Heater::ON).has_value()	  //
+										  && hdc1080.setHeater(HDC1080::Heater::OFF).has_value(); // Only on briefly.
+
+		bool setConfigSuccess = setConfigAcquisitionModeSuccess		  //
+							 && setConfigTemperatureResolutionSuccess //
+							 && setConfigHumidityResolutionSuccess	  //
+							 && setConfigHeaterEnabledSuccess;
+
+		float temperatureConfigured = hdc1080.getTemperature();
+		float humidityConfigured	= hdc1080.getHumidity();
+
+		const float temperatureMarginCelsius = 0.8f;
+		const float humidityMarginPercent	 = 1.0f;
+		bool getRegistersSuccessConfigured = std::abs(temperatureConfigured - temperature) < temperatureMarginCelsius //
+										  && std::abs(humidityConfigured - humidity) < humidityMarginPercent;
+
+		if (getRegistersSuccessDefaultConfig && setConfigSuccess && getRegistersSuccessConfigured) {
 			HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
 			HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
 			HAL_GPIO_TogglePin(LED3_GPIO_Port, LED3_Pin);
 		} else {
 			HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
-			HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
 			HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_SET);
 		}
 
+		hdc1080.softReset();
 		HAL_Delay(500);
 	}
 }
