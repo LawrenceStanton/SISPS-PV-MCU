89a0288e75c7d4aff4f1c397d5bc9ef157cda6f5
Lawrence Stanton <52958935+LawrenceStanton@users.noreply.github.com>
2023-07-12 11:39:46 +0200

    Add TMP116 I2C Library to Lib

---
+++ Lib/TMP116/Inc/TMP116_I2C.hpp
@@ -0,0 +1,23 @@
+/**
+ ******************************************************************************
+ * @file			: TMP116_I2C.hpp
+ * @brief			: I2C interface for the TMP116.
+ * @author			: Lawrence Stanton
+ ******************************************************************************
+ */
+
+#pragma once
+
+#include "TMP116.hpp"
+
+#include "stm32g0xx_hal.h"
+
+class I2C_TMP116 : public TMP116::I2C {
+	I2C_HandleTypeDef *hi2c;
+
+public:
+	virtual std::optional<Register> read(DeviceAddress deviceAddress, MemoryAddress address) final;
+	virtual std::optional<Register> write(DeviceAddress deviceAddress, MemoryAddress address, Register data) final;
+
+	I2C_TMP116(I2C_HandleTypeDef *hi2c) : hi2c(hi2c) {}
+};
---
+++ Lib/TMP116/Src/TMP116_I2C.cpp
@@ -0,0 +1,53 @@
+/**
+ ******************************************************************************
+ * @file			: TMP116_I2C.cpp
+ * @brief			: Source for TMP116_I2C.hpp
+ * @author			: Lawrence Stanton
+ ******************************************************************************
+ */
+
+#include "TMP116_I2C.hpp"
+
+using I2C			= I2C_TMP116;
+using Register		= TMP116::I2C::Register;
+using MemoryAddress = TMP116::I2C::MemoryAddress;
+using DeviceAddress = TMP116::I2C::DeviceAddress;
+
+std::optional<Register> I2C_TMP116::read(DeviceAddress deviceAddress, MemoryAddress address) {
+	uint8_t data[sizeof(Register)];
+	auto	resultCode = HAL_I2C_Mem_Read(
+		   this->hi2c,								 //
+		   static_cast<uint8_t>(deviceAddress) << 1, // I2C Address. Left shift required by HAL.
+		   address,									 // Memory Address to Read
+		   sizeof(MemoryAddress),					 //
+		   data,									 //
+		   sizeof(data),							 //
+		   10										 // milliseconds
+	   );
+
+	if (resultCode != HAL_OK) return std::nullopt;
+
+	Register result = data[0] << 8 | data[1];
+	return result;
+}
+
+std::optional<Register> I2C_TMP116::write(DeviceAddress deviceAddress, MemoryAddress address, Register data) {
+	uint8_t dataBytes[] = {
+		static_cast<uint8_t>(data >> 8),  // MSB First
+		static_cast<uint8_t>(data & 0xFF) // LSB Second
+	};
+
+	auto resultCode = HAL_I2C_Mem_Write(
+		this->hi2c,								  //
+		static_cast<uint8_t>(deviceAddress) << 1, // I2C Address. Left shift required by HAL.
+		address,								  // Memory Address to Write
+		sizeof(MemoryAddress),					  //
+		dataBytes,								  //
+		sizeof(data),							  //
+		10										  // milliseconds
+	);
+
+	if (resultCode != HAL_OK) return std::nullopt;
+
+	return data;
+}
