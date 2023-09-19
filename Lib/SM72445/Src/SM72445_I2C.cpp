de027e1c5335a60f15fa9306603ba7cb3d754e1f
Lawrence Stanton <52958935+LawrenceStanton@users.noreply.github.com>
2023-09-19 18:38:16 +0200

    Update SM72445 I2C Implementation. Bug fix: add length byte and byte packing into package.

--- Lib/SM72445/Src/SM72445_I2C.cpp
+++ Lib/SM72445/Src/SM72445_I2C.cpp
@@ -20,12 +20,15 @@ static_assert(sizeof(Register) == 8, "Register expected to be 8 bytes long.");
 static_assert(sizeof(MemoryAddress) == 1, "MemoryAddress expected to be 1 byte long.");
 static_assert(sizeof(DeviceAddress) == 1, "DeviceAddress expected to be 1 byte long.");
 
-optional<Register> SM72445_I2C::read(DeviceAddress deviceAddress, MemoryAddress memoryAddress) {
+optional<Register> SM72445_I2C::read(
+	DeviceAddress deviceAddress, //
+	MemoryAddress memoryAddress
+) {
 	uint8_t data[8]; // 7 bytes plus 1 length byte.
 
 	auto resultCode = HAL_I2C_Mem_Read(
 		this->hi2c,								  //
-		static_cast<uint8_t>(deviceAddress) << 1, // I2C Address. Left shift required by HAL.
+		static_cast<uint8_t>(deviceAddress) << 1, // I2C Address. Shift required by HAL.
 		static_cast<uint8_t>(memoryAddress),	  // Memory Address to Read
 		sizeof(MemoryAddress),					  //
 		data,									  //
@@ -46,16 +49,22 @@ optional<Register> SM72445_I2C::read(DeviceAddress deviceAddress, MemoryAddress 
 	return result;
 }
 
-optional<Register> SM72445_I2C::write(DeviceAddress deviceAddress, MemoryAddress memoryAddress, Register data) {
+optional<Register> SM72445_I2C::write(
+	DeviceAddress deviceAddress,
+	MemoryAddress memoryAddress,
+	Register	  data
+) {
 	uint8_t dataBuffer[8]; // 7 bytes plus 1 length byte.
 
-	for (auto i = 0u; i < sizeof(dataBuffer); i++) {
-		dataBuffer[i] = static_cast<uint8_t>((data >> (i * 8)) & 0xFFu);
+	dataBuffer[0] = 7; // Fixed length of all frames sent to SM72445.
+
+	for (auto i = 0u; i < sizeof(dataBuffer) - 1; i++) {
+		dataBuffer[i + 1] = static_cast<uint8_t>((data >> (i * 8)) & 0xFFu);
 	}
 
 	auto resultCode = HAL_I2C_Mem_Write(
 		this->hi2c,								  //
-		static_cast<uint8_t>(deviceAddress) << 1, // I2C Address. Left shift required by HAL.
+		static_cast<uint8_t>(deviceAddress) << 1, // I2C Address. Shift required by HAL.
 		static_cast<uint8_t>(memoryAddress),	  // Memory Address to Write
 		sizeof(MemoryAddress),					  //
 		dataBuffer,								  //
